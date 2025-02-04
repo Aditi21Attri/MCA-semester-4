1b17ff813d14762fddb36fb4bacb807ce83feb01
Aditi21Attri <aditiattri21@gmail.com>
2025-02-04 20:51:48 +0530

    sorting operation done

---
+++ arraysorting.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+using namespace std;
+
+// Function for Bubble Sort
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+// Function for Selection Sort
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIdx = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIdx]) {
+                minIdx = j;
+            }
+        }
+        swap(arr[i], arr[minIdx]);
+    }
+}
+
+// Function for Insertion Sort
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+// Function to display array
+void displayArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int n, choice;
+    
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    int arr[n];
+    
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    
+    cout << "Choose a sorting method:\n";
+    cout << "1. Bubble Sort\n";
+    cout << "2. Selection Sort\n";
+    cout << "3. Insertion Sort\n";
+    cout << "Enter your choice: ";
+    cin >> choice;
+    
+    switch (choice) {
+        case 1:
+            bubbleSort(arr, n);
+            break;
+        case 2:
+            selectionSort(arr, n);
+            break;
+        case 3:
+            insertionSort(arr, n);
+            break;
+        default:
+            cout << "Invalid choice!";
+            return 1;
+    }
+    
+    cout << "Sorted array: ";
+    displayArray(arr, n);
+    
+    return 0;
+}
